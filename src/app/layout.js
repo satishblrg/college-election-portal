cf977f8472ffe945ae3ca8d794d924443131be80
Satish <satishblrg@gmail.com>
2026-05-24 15:21:11 +0530

    Added favicon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "IAGI Election Portal",
   description: "Indian Academy Election Portal",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
